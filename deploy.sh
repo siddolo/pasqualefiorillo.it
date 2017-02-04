48a6b0899aaeeeae3284de5c7e78e18a76c84d43
Pasquale Fiorillo <info@pasqualefiorillo.it>
2017-02-04 19:31:20 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -21,15 +21,19 @@ PACKAGE="${PACKAGE_BASE_NAME}-${RELEASE}.tar.bz2"
 
 echo "$(date +'%Y-%m-%d %H:%M:%S') Creating ${PACKAGE} ..."
 git archive --format=tar --prefix=${DIRECTORY}/ ${RELEASETAG} | bzip2 -9 > ${PACKAGE}
+
 echo "$(date +'%Y-%m-%d %H:%M:%S') Uploading ${PACKAGE} to ssh://${DEPLOY_USER}@${DEPLOY_SERVER} ..."
 cat "${PACKAGE}" | ssh ${DEPLOY_USER}@${DEPLOY_SERVER} "sudo -u ${SUDO_USER} tar xj -C ${DEPLOY_PATH}"
+
 echo "$(date +'%Y-%m-%d %H:%M:%S') Enforce ownership and permission ..."
 ssh ${DEPLOY_USER}@${DEPLOY_SERVER} "sudo -u ${SUDO_USER} chown ${ENFORCE_OWNERSHIP} -R ${DEPLOY_PATH}${DIRECTORY}"
 ssh ${DEPLOY_USER}@${DEPLOY_SERVER} "sudo -u ${SUDO_USER} chmod ${ENFORCE_PERMISSION} -R ${DEPLOY_PATH}${DIRECTORY}"
+
 echo "$(date +'%Y-%m-%d %H:%M:%S') Linking the document root to the current package  ${DEPLOY_PATH}${DIRECTORY}/http ..."
 ssh ${DEPLOY_USER}@${DEPLOY_SERVER} "sudo -u ${SUDO_USER} rm ${DEPLOY_PATH}www"
 ssh ${DEPLOY_USER}@${DEPLOY_SERVER} "sudo -u ${SUDO_USER} ln -s ${DEPLOY_PATH}${DIRECTORY}/http ${DEPLOY_PATH}www"
 
 echo "$(date +'%Y-%m-%d %H:%M:%S') Removing ${PACKAGE} ..."
 rm "${PACKAGE}"
+
 echo "$(date +'%Y-%m-%d %H:%M:%S') Done"
